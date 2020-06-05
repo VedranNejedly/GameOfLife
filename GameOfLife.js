25a949305ad3d1ea0de7356889309f34b755fd8e
Vedran Nejedly <62423202+VedranNejedly@users.noreply.github.com>
2020-06-05 15:31:38 +0200

    Add files via upload

---
+++ GameOfLife.js
@@ -0,0 +1,75 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+
+canvas.height=400;
+canvas.width=400;
+const COLS= 400;
+const ROWS = 400;
+
+
+
+function buildGrid() {
+  return new Array(COLS).fill(null)
+    .map(() => new Array(ROWS).fill(null)
+      .map(() => Math.floor(Math.random() * 2)));
+}
+
+let grid = buildGrid();
+
+requestAnimationFrame(update);
+
+function update() {
+  grid = nextGen(grid);
+  render(grid);
+  requestAnimationFrame(update);
+}
+
+function nextGen(grid) {
+  const nextGen = grid.map(arr => [...arr]);
+
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[col].length; row++) {
+      const cell = grid[col][row];
+      let numNeighbours = 0;
+      for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+          if (i === 0 && j === 0) {
+            continue;
+          }
+          const x_cell = col + i;
+          const y_cell = row + j;
+
+          if (x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
+            const currentNeighbour = grid[col + i][row + j];
+            numNeighbours += currentNeighbour;
+          }
+        }
+      }
+
+      // rules
+      if (cell === 1 && numNeighbours < 2) {
+        nextGen[col][row] = 0;
+      } else if (cell === 1 && numNeighbours > 3) {
+        nextGen[col][row] = 0;
+      } else if (cell === 0 && numNeighbours === 3) {
+        nextGen[col][row] = 1;
+      }
+    }
+  }
+  return nextGen;
+}
+
+function render(grid) {
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[col].length; row++) {
+      const cell = grid[col][row];
+
+      ctx.beginPath();
+      ctx.rect(col, row, 400, 400);
+      ctx.fillStyle = cell ? 'red' : 'white';
+      ctx.fill();
+      // ctx.stroke();
+    }
+  }
+}
